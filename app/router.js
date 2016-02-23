a06f5b3737bc17aa186b7f45f5f067a3efbbe48a
David Wilhelm <davwilh@gmail.com>
2016-02-22 20:06:05 -0500

    Remove demo

--- app/router.js
+++ app/router.js
@@ -9,7 +9,6 @@ Router.map(function() {
   this.route('home', function() {
     this.route('add');
   });
-  // this.route('demo', function() {});
   this.route('signup');
   this.route('reset');
 });
